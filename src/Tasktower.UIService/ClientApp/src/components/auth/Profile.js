b1065446e8b95832309cf3d9c5d7d00638d56b1a
obmeir <orenbm11@gmail.com>
2021-02-18 00:54:36 -0500

    added

---
+++ src/Tasktower.UIService/ClientApp/src/components/auth/Profile.js
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Profile = () => {
+    const { user, isAuthenticated, isLoading, getAccessTokenSilently, getIdTokenClaims } = useAuth0();
+
+    if (isLoading) {
+        return <div>Loading ...</div>;
+    }
+    if (!isAuthenticated) {
+        return (<div>401 unauthorized</div>)
+    }
+    getAccessTokenSilently({ ignoreCache: true, scope: "openid email profile" })
+        .then(console.log).catch(console.error);
+    getIdTokenClaims({ ignoreCache: true, scope: "openid email profile" })
+        .then(x => console.log(x.__raw)).catch(console.error);
+    return (
+        isAuthenticated && (
+            <div>
+                <img src={user.picture} alt={user.name} />
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                <p>{JSON.stringify(user)}</p>
+            </div>
+        )
+    );
+};
+
+export default Profile;
---
+++ src/Tasktower.UIService/ClientApp/src/index.js
@@ -0,0 +1,27 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
+import registerServiceWorker from './registerServiceWorker';
+
+const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+    <BrowserRouter basename={baseUrl}>
+        {/*domain = "dev-cl8od7hx.us.auth0.com"*/}
+        <Auth0Provider
+            domain="dev-cl8od7hx.us.auth0.com"
+            clientId="OSafhsQn1OiTPAsuh79GZY04O7SaIAoe"
+            redirectUri={window.location.origin}
+            scope="openid email profile"
+        >
+            <App />
+        </ Auth0Provider>
+  </BrowserRouter>,
+  rootElement);
+
+registerServiceWorker();
+
