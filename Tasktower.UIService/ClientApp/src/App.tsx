09ed33bc88a11c329c277571f123e66e3d0487c3
Oren Ben-Meir <orenbm11@gmail.com>
2021-05-30 05:30:28 -0400

    react setup

--- Tasktower.UIService/ClientApp/src/App.tsx
+++ Tasktower.UIService/ClientApp/src/App.tsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useCookies} from "react-cookie";
+
 import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  const [cookies] = useCookies(['XSRF-TOKEN']);
+  
+  const login = async () => {
+    try {
+      let r = await fetch("/client/auth/login?returnUrl=" + encodeURI(window.origin + "/"), {
+        method: "get",
+        credentials: "include",
+        headers: {
+          "X-XSRF-TOKEN": cookies["XSRF-TOKEN"]
+        },
+        redirect: "manual"
+      });
+      console.log(r)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const logout = async () => {
+    try {
+      let r = await fetch("/client/auth/logout?returnUrl=" + encodeURI(window.origin + "/"), {
+        method: "get",
+        credentials: "include",
+        headers: {
+          "X-XSRF-TOKEN": cookies["XSRF-TOKEN"]
+        },
+        redirect: "manual"
+      });
+      console.log(r)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
+  fetch("https://localhost:9090/services/project/api/test/auth", {credentials: "include"})
+    .then(r => r.json())
+    .then(r => console.log(r))
+    .catch(e => console.log(e))
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +60,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <form action={"/client/auth/login"} method="get">
+        <input name="returnUrl" type="hidden" value={encodeURI(window.origin + "/")}/>
+        <input name="submit" type="submit" value="login"/>
+      </form>
+      <form action={"/client/auth/logout"} method="get">
+        <input name="returnUrl" type="hidden" value={encodeURI(window.origin + "/")}/>
+        <input name="submit" type="submit" value="logout"/>
+      </form>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
